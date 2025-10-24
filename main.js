77eccfa296e1641a3f2ff13158408999b148b86a
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2025-10-24 01:09:41 -0300

    feat: html base

---
+++ main.js
@@ -0,0 +1,22 @@
+const intersectionCallback = (entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            let animation = entry.target.getAttribute('data-animation');
+            let delay = entry.target.getAttribute('data-delay');
+            entry.target.classList.add(animation);
+            delay && entry.target.classList.add('animate__delay-' + delay + 's');
+        }
+    }
+}
+
+const observer = new IntersectionObserver(intersectionCallback);
+
+const items = document.querySelectorAll('.animate__animated');
+
+for (const item of items) {
+    observer.observe(item);
+}
+
+const nav = document.getElementById("navbar");
+
+window.addEventListener( "scroll", () => (window.scrollY >= 250) ? nav.classList.remove('py-4') : nav.classList.add('py-4'))
